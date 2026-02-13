84c399b4d66bd927a22791624c56ffd2e1a7ff21
MacThas <ovenmac@gmail.com>
2026-02-13 10:29:00 +0700

    #

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,28 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+void stat(const double S[],int N,double M[]){
+    double sum = 0.0;
+    double sum2 = 0.0;
+    double allx = 1.0;
+    double ally = 0.0;
+    double max = S[0];
+    double min = S[0];
+
+    for(int i = 0; i < N; i++){
+        sum += S[i];
+        sum2 += S[i] * S[i];
+        allx *= S[i];
+        ally += 1.0 / S[i];
+        if(S[i] > max) max = S[i];
+        if(S[i] < min) min = S[i];
+    }
+
+    M[0] = sum / N;
+    M[1] = sqrt((sum2 / N) - (M[0]* M[0]));
+    M[2] = pow(allx, 1.0 / N);
+    M[3] = N / ally;
+    M[4] = max;
+    M[5] = min;
+}
